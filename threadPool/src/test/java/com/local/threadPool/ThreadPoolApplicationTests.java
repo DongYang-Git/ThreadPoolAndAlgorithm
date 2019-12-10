f2f177a32747d9c245d993ed9237d5a2a533f6a7
dongyang <yang.dong@yuxiaor.com>
2019-12-10 11:53:37 +0800

    导入基本项目

---
+++ threadPool/src/test/java/com/local/threadPool/ThreadPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.local.threadPool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadPoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
